87563b342d505689ae0110f9306e9e310c437c85
Suyog Gautam <sakurachhetri97@gmail.com>
2026-01-28 17:13:13 +0545

    initial commit

---
+++ filehandlinginnode/file.js
@@ -0,0 +1,22 @@
+
+const filesystem = require("fs")// so fs is a builtin module in node which helps to interact with files
+
+// filesystem.writeFileSync('./suyogtest.txt','a bright future');
+// //this will create a file named suyogtest in the same directory
+// filesystem.writeFile('./suyogtest.txt','great day',(err)=>{});
+// //async in this we need to provide a callback for errors
+
+// const read = filesystem.readFileSync('./reading.txt','utf-8');
+// console.log(read);
+
+//here in async we dont have a return type
+filesystem.readFile('./reading.txt','utf-8',(err,readresult)=>{
+    if(err){
+        console.log("Error is ",err);
+        
+    }
+    else{
+        console.log(readresult);
+        
+    }
+})
---
+++ modules/main.js
@@ -0,0 +1,5 @@
+const maths = require("./math")
+
+console.log("the addition is ", maths.add(1,2));
+console.log("the substraction is ",maths.sub(3,2));
+
