004c1c6e8376690fddde69d6b44589902cad9033
Suyog Gautam <sakurachhetri97@gmail.com>
2026-01-31 12:57:31 +0545

    analyzingcpu intensive tasks using crypt module

--- nodejsworking/index.js
+++ nodejsworking/index.js
@@ -1,16 +1,17 @@
-const { log } = require('console');
 const fs = require('fs')
+const crypto = require('crypto');
 //goes through event loop 
+
+process.env.UV_THREADPOOL_SIZE=5; //default is 4
 setTimeout(() => {
     console.log('hello i am the timer')
 }, 0);
 
-
+const start = Date.now();
 setImmediate(()=>{
     console.log('hello i am immediate fn');
     
 })
-
 
 fs.readFile('sample.txt','UTF-8',()=>{
     console.log('hello io polling ');
@@ -26,6 +27,25 @@ fs.readFile('sample.txt','UTF-8',()=>{
         console.log('immediatefn');
         
     })
+
+    crypto.pbkdf2('password1','salt',100000,512,'sha512',()=>{
+        console.log(`${Date.now() - start}`,'password1 encrypted');
+    });
+     crypto.pbkdf2('password2','salt',100000,512,'sha512',()=>{
+        console.log(`${Date.now() - start}`,'password2 encrypted');
+    });
+     crypto.pbkdf2('password3','salt',100000,512,'sha512',()=>{
+        console.log(`${Date.now() - start}`,'password3 encrypted');
+    });
+     crypto.pbkdf2('password4','salt',100000,512,'sha512',()=>{
+        console.log(`${Date.now() - start}`,'password4 encrypted');
+    });
+     crypto.pbkdf2('password5','salt',100000,512,'sha512',()=>{
+        console.log(`${Date.now() - start}`,'password5 encrypted');
+    });
+
+    // here the threadpool size is 4 by default so only 4 crypto functions will run parallelly and the 5th one waits for its turn
+
 })
 //top level code
 
