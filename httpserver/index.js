28b4517c9360f0e97df76f541e28c48d763da340
Suyog Gautam <sakurachhetri97@gmail.com>
2026-02-02 23:03:01 +0545

    getting started with express

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,27 +1,46 @@
-const http = require("http");
-const fs = require("fs");
-const server = http.createServer((req, res) => {
-  console.log(req.headers);
-  const log = `${Date.now()}: ${req.url} New req made \n `;
+// const http = require("http");
+// const fs = require("fs");
+// const server = http.createServer((req, res) => {
+//   console.log(req.headers);
+//   const log = `${Date.now()}: ${req.url} New req made \n `;
 
-  fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
-      case "/":
-        res.end("Welcome to the homepage");
-        break;
-      case "/contact":
-        res.end("contact-page");
-        break;
-      case "/about":
-        res.end("aboutpage");
-        break;
-      default:
-        res.end("404-page not found");
+//   fs.appendFile("log.txt", log, (err, data) => {
+//     switch (req.url) {
+//       case "/":
+//         res.end("Welcome to the homepage");
+//         break;
+//       case "/contact":
+//         res.end("contact-page");
+//         break;
+//       case "/about":
+//         res.end("aboutpage");
+//         break;
+//       default:
+//         res.end("404-page not found");
         
-    }
-  });
-});
+//     }
+//   });
+// });
 
-server.listen(8000, () => {
-  console.log("hello from the server");
-});
+// server.listen(8000, () => {
+//   console.log("hello from the server");
+// });
+
+
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res)=>{
+  return res.end('hello from express');
+})
+
+app.get('/about',(req,res)=>{
+ return  res.end(`hello ${req.query.name} from about page`);
+})
+
+app.listen(8000,()=>{
+  console.log('server started on port 8000');
+})
+
+// so here express is built on top of nodejs http module and it makes our work easier by providing us with many inbuilt functions and middlewares to handle requests and responses
