ada873df360277b0ee650343534a422fafd70847
Suyog Gautam <sakurachhetri97@gmail.com>
2026-02-04 22:26:02 +0545

    using otherhttpmethods(put,patch,delete)

--- Restapi/index.js
+++ Restapi/index.js
@@ -1,30 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const users = require("./MOCK_DATA.json")
+const users = require("./MOCK_DATA.json");
 
 //raw data in json format for frontend consumption
-app.get('/api/users',(req,res)=>{
- return res.json(users);
-})
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
 
 //html data for server side rendering
-app.get('/users',(req,res)=>{
-    const html=
-    
-    `
+app.get("/users", (req, res) => {
+  const html = `
     <ul>
-        ${users.map((user)=>{
+        ${users
+          .map((user) => {
             return `<li> ${user.first_name} ${user.last_name} </li>`;
-        }).join('')}
+          })
+          .join("")}
 
     </ul>
     
-    `
-res.send(html);
-    
-})
+    `;
+  res.send(html);
+});
+app.route("/api/users/:id").get((req, res) => {
+  //using dynamic route parameter
 
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port      }`);
+  const id = Number(req.params.id);
+  const user = users.find((u) => u.id === id);
+  return res.json(user);
+}).patch((req,res)=>{
+    return res.send("PATCH request received for user with id: " + req.params.id);
 })
+.put((req,res)=>{
+    return res.send("PUT request received for user with id: " + req.params.id);
+})
+.delete((req,res)=>{
+    return res.send("DELETE request received for user with id: " + req.params.id);
+});
+
+
+app.post("/api/users", (req, res) => {
+    return res.send("POST request received to create a new user");
+});
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
