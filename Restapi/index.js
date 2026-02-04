aeba7d9fe8d6efb61b46612ea4a0f03ac312e6cb
Suyog Gautam <sakurachhetri97@gmail.com>
2026-02-04 17:11:03 +0545

    ways of sendingg the data

--- Restapi/index.js
+++ Restapi/index.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const users = require("./MOCK_DATA.json")
 
+//raw data in json format for frontend consumption
+app.get('/api/users',(req,res)=>{
+ return res.json(users);
+})
+
+//html data for server side rendering
+app.get('/users',(req,res)=>{
+    const html=
+    
+    `
+    <ul>
+        ${users.map((user)=>{
+            return `<li> ${user.first_name} ${user.last_name} </li>`;
+        }).join('')}
+
+    </ul>
+    
+    `
+res.send(html);
+    
+})
 
 app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port      }`);
 })
