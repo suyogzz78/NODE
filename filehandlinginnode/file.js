6b5feea54f5aeecb1d74a048f8d810fe4ca213f0
Suyog Gautam <sakurachhetri97@gmail.com>
2026-02-04 22:39:00 +0545

    syncvsasync

--- filehandlinginnode/file.js
+++ filehandlinginnode/file.js
@@ -1,13 +1,13 @@
 
 const filesystem = require("fs")// so fs is a builtin module in node which helps to interact with files
 
-// filesystem.writeFileSync('./suyogtest.txt','a bright future');
-// //this will create a file named suyogtest in the same directory
-// filesystem.writeFile('./suyogtest.txt','great day',(err)=>{});
-// //async in this we need to provide a callback for errors
+filesystem.writeFileSync('./suyogtest.txt','a bright future');
+//this will create a file named suyogtest in the same directory
+filesystem.writeFile('./suyogtest.txt','great day',(err)=>{});
+//async in this we need to provide a callback for errors
 
-// const read = filesystem.readFileSync('./reading.txt','utf-8');
-// console.log(read);
+const read = filesystem.readFileSync('./reading.txt','utf-8');
+console.log(read);
 
 //here in async we dont have a return type
 filesystem.readFile('./reading.txt','utf-8',(err,readresult)=>{
@@ -20,3 +20,6 @@ filesystem.readFile('./reading.txt','utf-8',(err,readresult)=>{
         
     }
 })
+
+filesystem.appendFileSync("./reading.txt",`${Date.now()} Hello There \n`);
+filesystem.unlinkSync("./test.txt")
