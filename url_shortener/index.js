ec514c2c2770599e88249f6a34069bdfaf4a72b5
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-04 17:22:09 +0545

    main file for urlshortening

--- url_shortener/index.js
+++ url_shortener/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const PORT = 3000;
+const PORT = 8000;
 const app = express();
+const urlroute = require('./routes/url');
+const {connecttodatabase} = require('./connection');
+//middleware
+app.use(express.json());
+connecttodatabase('mongodb://localhost:27017/url_shortener').then(()=>{
+    console.log('Connected to database');
+})
 
+app.use("/url",urlroute);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
