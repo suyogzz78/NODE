649552f44b715301fccad3ad88d11a735b6c1170
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-04 17:23:56 +0545

    feat:models separation of mvc

---
+++ url_shortener/models/url.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+
+const urlSchema = new mongoose.Schema({
+    shortId:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    redirectUrl:{
+        type:String,
+        required:true
+    },
+    viewHistory:[{ timestamp: { type: Date, default: Date.now } }]
+
+},{timestamps:true});
+
+
+const URL = new mongoose.model('URL',urlSchema);
+
+module.exports = URL;
