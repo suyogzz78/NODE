0e80d6f8a96fdfd16abf09c6e824a5d5345b2074
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-05 22:03:12 +0545

    addedgetmethod

--- url_shortener/routes/url.js
+++ url_shortener/routes/url.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const {handlecreateShortUrl} = require('../controllers/url');
 
 router.post('/',handlecreateShortUrl);
+router.get('/:shortId',async (req,res)=>{   
+    const shortId = req.params.shortId;
+    const url = await URL.findOne({
+        shortId
+    });
+});
 
 
 module.exports =router;
