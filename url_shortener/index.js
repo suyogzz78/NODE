ec10af1b92924f1c0a046d22971479485da97d63
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-08 22:17:06 +0545

    used ejs to separate the html docs

--- url_shortener/index.js
+++ url_shortener/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const PORT = 8000;
 const app = express();
+const path = require('path');
 const urlroute = require('./routes/url');
+const URL = require('./models/url');
 const {connecttodatabase} = require('./connection');
 //middleware
 app.use(express.json());
@@ -9,8 +11,14 @@ connecttodatabase('mongodb://localhost:27017/url_shortener').then(()=>{
     console.log('Connected to database');
 })
 
-app.use("/url",urlroute);
-
+ app.set('view engine','ejs');//using ejs as template engine for rendering html pages
+ app.set('views',path.resolve("./views"));//setting the views directory for ejs templates
+ app.use("/url",urlroute);
+app.get('/test',async(req,res)=>
+{
+    const allUrls = await URL.find({});
+   return res.render('homepage',{urls:allUrls});
+})
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
 });
