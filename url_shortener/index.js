f196eba9c6a4f5dc147eb53a9a1171fae358a9ec
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-09 17:50:40 +0545

    added new routes

--- url_shortener/index.js
+++ url_shortener/index.js
@@ -5,8 +5,10 @@ const path = require('path');
 const urlroute = require('./routes/url');
 const URL = require('./models/url');
 const {connecttodatabase} = require('./connection');
+const staticroute = require('./routes/staticroute');
 //middleware
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 connecttodatabase('mongodb://localhost:27017/url_shortener').then(()=>{
     console.log('Connected to database');
 })
@@ -14,11 +16,12 @@ connecttodatabase('mongodb://localhost:27017/url_shortener').then(()=>{
  app.set('view engine','ejs');//using ejs as template engine for rendering html pages
  app.set('views',path.resolve("./views"));//setting the views directory for ejs templates
  app.use("/url",urlroute);
-app.get('/test',async(req,res)=>
-{
-    const allUrls = await URL.find({});
-   return res.render('homepage',{urls:allUrls});
-})
+ app.use('/',staticroute);
+// app.get('/test',async(req,res)=>
+// {
+//     const allUrls = await URL.find({});
+//    return res.render('homepage',{urls:allUrls});
+// })
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
-});
+}); 
