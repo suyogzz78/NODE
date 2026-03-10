0d0997c0695983259f82e38551dda6b3729df557
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-10 15:42:19 +0545

    featroute:fixedthe issue with id

--- url_shortener/routes/staticroute.js
+++ url_shortener/routes/staticroute.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const URL = require('../models/url');
+const shortid = require('shortid');
 
 router.get('/',async (req,res)=>{
+    // const shortID = shortid();
+    // await URL.create({
+    //     shortId:shortID,
+    //     redirectUrl:'https://www.google.com',
+    //     viewHistory:[]
+    // });
     const allUrls = await URL.find({});
-    res.render('homepage',{
+    return  res.render('homepage',{
+        // id:shortID,
         urls:allUrls
     });
 })
