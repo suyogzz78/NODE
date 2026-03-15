7c16625bc56e2f76e6bf032925633fdf5768e75e
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-15 18:23:27 +0545

    added sessionid using uuid for cookies

--- url_shortener/controllers/user.js
+++ url_shortener/controllers/user.js
@@ -1,25 +1,34 @@
-const User = require("../models/user")
-const {v4:uuidv4} = require('uuid');
-async function handleSignup(req,res){
-    const {name,email,password} = req.body;
-    await User.create(
-        {
-            name,email,password
-        }
-    );
- 
- return res.redirect('/');
+const User = require("../models/user");
+const { v4: uuidv4 } = require("uuid");
+const { setUser } = require("../utils/auth");
+
+async function handleSignup(req, res) {
+  const { name, email, password } = req.body;
+  await User.create({
+    name,
+    email,
+    password,
+  });
+
+  return res.redirect("/");
 }
 
-async function handleLogin(req,res){
-    const {email,password} = req.body;
-    const user = await User.findOne({email,password});
-        if(!user){
-            return res.render('login',{error:'Invalid email or password. Please try again.'});
-        }
- return res.redirect('/');    
+async function handleLogin(req, res) {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email, password });
+  if (!user) {
+    return res.render("login", {
+      error: "Invalid email or password. Please try again.",
+    });
+  }
 
+  const sessionId = uuidv4();
+  setUser(sessionId, user);
+  res.cookie("sessionId", sessionId);
 
+  //here the sessionId can be stored in a database or in-memory store to manage user sessions. For simplicity, we are just setting a cookie here.
+
+  return res.redirect("/");
 }
 
-module.exports = {handleSignup,handleLogin};
+module.exports = { handleSignup, handleLogin };
