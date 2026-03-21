65615414cf74bcaeda94c37a711f3f7f5115e5d0
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-21 20:38:31 +0545

    using headers instead of cookies for tokens

--- url_shortener/middlewares/auth.js
+++ url_shortener/middlewares/auth.js
@@ -1,31 +1,73 @@
+// const { getUser } = require("../utils/auth");
+
+// async function authMiddleware(req, res, next) {
+//   const userid = req.headers["authorization"]; // Assuming the token is sent in the format "Bearer <token>"
+
+//   if (!userid) {
+//     return res.status(401).redirect("/login");
+//   }
+//   const token = userid.split("Bearer ")[1];
+//   const user = getUser(token);
+
+//   if (!user) {
+//     return res.status(401).redirect("/login");
+//   }
+//   req.user = user;
+
+//   next();
+// }
+
+// async function checkauthMiddleware(req, res, next) {
+//   // const token = req.cookies.token;
+//   //  const user = getUser(token);
+
+//   //  req.user = user;//if user is not found, req.user will be undefined, which can be handled in the route handlers to allow access to public routes without authentication.
+//   // next();
+
+//   const userid = req.headers["authorization"]; // Assuming the token is sent in the format "Bearer <token>"
+//    if (!userid) {
+//     return res.status(401).redirect("/login");
+//   }
+//   const token = userid.split("Bearer ")[1]; // Extract the token part from the "Bearer <token
+//   const user = getUser(token);
+//   req.user = user;
+//   next();
+// }
+// module.exports = { authMiddleware, checkauthMiddleware };
 const { getUser } = require("../utils/auth");
 
-
+// Middleware for protected routes (must be logged in)
 async function authMiddleware(req, res, next) {
+  const authHeader = req.headers["authorization"]; // may be undefined
 
-    const uid = req.cookies.sessionId;
+  if (!authHeader) {
+    return res.status(401).redirect("/login"); // no token → redirect
+  }
 
-    if (!uid){
-        return res.status(401).redirect('/login');
-    }
+  const token = authHeader.split(" ")[1]; // safe extraction
+  const user = getUser(token);
 
-    const user = getUser(uid);
+  if (!user) {
+    return res.status(401).redirect("/login"); // invalid token
+  }
 
-    if (!user){
-        return res.status(401).redirect('/login');
-    }
-req.user = user;
-
-    next();
+  req.user = user;
+  next();
 }
 
+// Middleware for optional auth (public + private routes)
 async function checkauthMiddleware(req, res, next) {
+  const authHeader = req.headers["authorization"];
 
-    const token = req.cookies.token;
-     const user = getUser(token);
+  if (!authHeader) {
+    req.user = undefined; // optional → allow public access
+    return next();
+  }
 
-     req.user = user;//if user is not found, req.user will be undefined, which can be handled in the route handlers to allow access to public routes without authentication.
-    next();
-
+  const token = authHeader.split(" ")[1];
+  const user = getUser(token);
+  req.user = user;
+  next();
 }
+
 module.exports = { authMiddleware, checkauthMiddleware };
