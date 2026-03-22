c2c3b0816060385a70f77edc8b438947c7bb3fa1
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-22 18:42:03 +0545

    authenticating using token through cookies

--- url_shortener/controllers/user.js
+++ url_shortener/controllers/user.js
@@ -25,13 +25,13 @@ async function handleLogin(req, res) {
 
 
   const token = setUser(user);
-  // res.cookie("token", token,{
+   res.cookie("token", token);
   //   domain: "localhost", // added domain for cookie 
   // });
 
   // //here the sessionId can be stored in a database or in-memory store to manage user sessions. For simplicity, we are just setting a cookie here.
 
-  return res.json({ token }); // Send the token in the response body as JSON
+  return res.redirect("/"); // Send the token in the response body as JSON
 }
 
 module.exports = { handleSignup, handleLogin };
