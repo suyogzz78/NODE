08c77428f8d571204edba7807abf8ea02b4b45bb
Suyog Gautam <sakurachhetri97@gmail.com>
2026-03-22 18:44:42 +0545

    added the admin roles and normal roles using the controller function in the necessary routes

--- url_shortener/routes/staticroute.js
+++ url_shortener/routes/staticroute.js
@@ -2,15 +2,26 @@ const express = require('express');
 const router = express.Router();
 const URL = require('../models/url');
 const shortid = require('shortid');
+const {restriction} = require('../middlewares/auth');
 
-router.get('/',async (req,res)=>{
+router.get('/admin/urls',restriction(['ADMIN']),async (req,res)=>{
+   
+   
+    const allUrls = await URL.find({});//updated to fetch only the URLs created by the logged-in user
+    return  res.render('homepage',{
+        // id:shortID,
+        urls:allUrls
+    });
+})
+
+router.get('/',restriction(['NORMAL','ADMIN']),async (req,res)=>{
     // const shortID = shortid();
     // await URL.create({
     //     shortId:shortID,
     //     redirectUrl:'https://www.google.com',
     //     viewHistory:[]
     // });
-    if(!req.user) return res.redirect('/login');//
+   
     const allUrls = await URL.find({createdBy:req.user._id});//updated to fetch only the URLs created by the logged-in user
     return  res.render('homepage',{
         // id:shortID,
